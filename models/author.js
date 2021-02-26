c2c5e35739f64335d7a3fda39e80a3babe7da67e
Amit Klein <AmitKlain1@gmail.com>
2021-02-26 17:30:59 +0200

    Finished Author Routes

--- models/author.js
+++ models/author.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
-
+const Book = require("./book");
 const AuthorShcema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
+});
+
+AuthorShcema.pre("remove", function (next) {
+  Book.find({ author: this.id }, (err, books) => {
+    if (err) {
+      next(err);
+    } else if (books.length > 0) {
+      next(new Error("This Author has books cant remove"));
+    } else {
+      next();
+    }
+  });
 });
 
 module.exports = mongoose.model("Author", AuthorShcema);
