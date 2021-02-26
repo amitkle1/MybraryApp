cee43ea813dcdfe54d27c733bf01894cc6317c12
Amit Klein <AmitKlain1@gmail.com>
2021-02-26 17:54:25 +0200

    updated

--- routes/books.js
+++ routes/books.js
@@ -2,26 +2,22 @@ const express = require("express");
 const router = express.Router();
 const Book = require("../models/book");
 const Author = require("../models/author");
-const imageMimeTypes = ["image/jpeg", "image/png", "image/gif"];
+const imageMimeTypes = ["image/jpeg", "image/png", "images/gif"];
 
-//All Books Route
+// All Books Route
 router.get("/", async (req, res) => {
   let query = Book.find();
-
   if (req.query.title != null && req.query.title != "") {
     query = query.regex("title", new RegExp(req.query.title, "i"));
   }
   if (req.query.publishedBefore != null && req.query.publishedBefore != "") {
     query = query.lte("publishDate", req.query.publishedBefore);
   }
-
   if (req.query.publishedAfter != null && req.query.publishedAfter != "") {
     query = query.gte("publishDate", req.query.publishedAfter);
   }
-
   try {
     const books = await query.exec();
-
     res.render("books/index", {
       books: books,
       searchOptions: req.query,
@@ -31,12 +27,12 @@ router.get("/", async (req, res) => {
   }
 });
 
-//New Book Route
+// New Book Route
 router.get("/new", async (req, res) => {
   renderNewPage(res, new Book());
 });
 
-//Create Book Route
+// Create Book Route
 router.post("/", async (req, res) => {
   const book = new Book({
     title: req.body.title,
@@ -59,7 +55,10 @@ router.post("/", async (req, res) => {
 async function renderNewPage(res, book, hasError = false) {
   try {
     const authors = await Author.find({});
-    const params = { authors: authors, book: book };
+    const params = {
+      authors: authors,
+      book: book,
+    };
     if (hasError) params.errorMessage = "Error Creating Book";
     res.render("books/new", params);
   } catch {
